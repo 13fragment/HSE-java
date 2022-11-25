ac995aa2fb8a5f6bb74d44714d142e9f1d01017a
4kith <mgolubevbiz@gmail.com>
2022-11-25 09:27:28 +0300

    refactor code

--- src/laboratotory_1/ex4.java
+++ src/laboratotory_1/ex4.java
@@ -11,24 +11,19 @@ public class ex4 {
         int kol_vo = input.nextInt();
         System.out.print("Рудиус окружности: ");
         float radius = input.nextFloat();
-
-            System.out.print("X 1 точки: ");
+        int counter = 0;
+        for (int i = 1; i <= kol_vo; i++) {
+            System.out.print("X Координата точки: ");
             float x1 = input.nextFloat();
 
-            System.out.print("Y 1 точки: ");
+            System.out.print("Y Координата точки: ");
+
             float y1 = input.nextFloat();
-
-            System.out.print("X 2 точки: ");
-            float x2 = input.nextFloat();
-
-            System.out.print("Y 2 точки: ");
-            float y2 = input.nextFloat();
-        int counter = 0;
-
-        input.close();
-        if((x1*x1+y1*y1<=radius*radius)|(x2*x2+y2*y2<=radius*radius)){
-            counter +=1;
+            if ((x1 * x1 + y1 * y1 <= radius * radius)) {
+                counter += 1;
+            }
         }
         System.out.println(counter + " точек попали в окружность");
+        input.close();
     }
 }
