836ef0975eb2746f5ea370cc17cc68d7dc1c0773
4kith <mgolubevbiz@gmail.com>
2022-11-25 08:39:47 +0300

    create logic

--- src/laboratotory_1/ex4.java
+++ src/laboratotory_1/ex4.java
@@ -6,24 +6,29 @@ public class ex4 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        Количество точек: 2
-//        Радиус окружности: 2
-//        X 1 точки: 10
-//        Y 1 точки: 12
-//        X 2 точки: 500
-//        Y 2 точки: -6
+
         System.out.print("Количество точек: ");
         int kol_vo = input.nextInt();
         System.out.print("Рудиус окружности: ");
         float radius = input.nextFloat();
-        System.out.print("X 1 точки: ");
-        float x1 = input.nextFloat();
-        System.out.print("Y 1 точки: ");
-        float y1 = input.nextFloat();
-        System.out.print("X 2 точки: ");
-        float x2 = input.nextFloat();
-        System.out.print("Y 2 точки: ");
-        float y2 = input.nextFloat();
+
+            System.out.print("X 1 точки: ");
+            float x1 = input.nextFloat();
+
+            System.out.print("Y 1 точки: ");
+            float y1 = input.nextFloat();
+
+            System.out.print("X 2 точки: ");
+            float x2 = input.nextFloat();
+
+            System.out.print("Y 2 точки: ");
+            float y2 = input.nextFloat();
+        int counter = 0;
+
         input.close();
+        if((x1*x1+y1*y1<=radius*radius)|(x2*x2+y2*y2<=radius*radius)){
+            counter +=1;
+        }
+        System.out.println(counter + " точек попали в окружность");
     }
 }
