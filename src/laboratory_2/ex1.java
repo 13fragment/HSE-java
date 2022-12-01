f2fc15b8d3160a4a5ce0e35ba8d518b0a9c55ae1
4kith <mgolubevbiz@gmail.com>
2022-12-01 21:34:38 +0300

    update logic

--- src/laboratory_2/ex1.java
+++ src/laboratory_2/ex1.java
@@ -18,7 +18,7 @@ public class ex1 {
         String s = input.next();
         input.close();
 
-        String Regexp = "[0-9]+/[0-9]+/[0-9]+";
+        String Regexp = "([0-9]|1[0-9]|2[0-9]|3[0-1])/([0-9]|1[0-2])/[0-9]+";
 
 
         input.close();
