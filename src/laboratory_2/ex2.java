43ca26520cf3aaa6e81c9930c4a838d744e4e2ef
4kith <mgolubevbiz@gmail.com>
2022-12-03 00:03:59 +0300

    update solution

--- src/laboratory_2/ex2.java
+++ src/laboratory_2/ex2.java
@@ -1,17 +1,28 @@
 package laboratory_2;
-
 import java.util.Scanner;
+import java.util.regex.*;
 public class ex2 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.next();
 
-        while (s.length()<8){
-            System.out.println("Пароль ненадежен. Повторите ввод");
-            s = in.next();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите пароль: ");
+
+        while (true) {
+            if (input.hasNextLine()){
+                String date = input.nextLine();
+                String pattern1 = "((?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[0-9a-zA-Z_]{8,})";
+                Pattern p1 = Pattern.compile(pattern1);
+                Matcher match1 = p1.matcher(date);
+                if (match1.matches()) {
+                    System.out.println("Пароль надёжный");
+                    System.exit(0);
+                }
+                else {
+
+                    System.out.println("Пароль ненадёжный"+"\n"+"Повторите ввод");
+                }
+            }
+
         }
-
-        in.close();
-        System.out.println("Пароль надежен");
     }
 }
