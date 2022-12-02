17a6f6276f17d26ddeb61f44347cbbea8c464c6f
4kith <mgolubevbiz@gmail.com>
2022-12-02 23:58:43 +0300

    update solution

--- src/laboratory_2/ex1.java
+++ src/laboratory_2/ex1.java
@@ -1,37 +1,40 @@
 package laboratory_2;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
+
 
 public class ex1 {
     public static void main(String[] args) {
 
-        class output{
-            static final String valid = "Введеное выражение является датой";
-            static final String invalidDate = "Введеное выражение не является датой";
-            static final String invalidFormat = "Введеное выражение не соответсвует заданому формату даты dd/mm/yyyy";
-        }
-
         Scanner input = new Scanner(System.in);
-        System.out.print("Дата: ");
-        String s = input.next();
-        input.close();
-
-        String Regexp = "([0-9]|1[0-9]|2[0-9]|3[0-1])/([0-9]|1[0-2])/((19[0-9][0-9])|([2-9][0-9][0-9][0-9]))";
-
-
-        input.close();
-
-        Pattern p = Pattern.compile(Regexp);
-        Matcher m = p.matcher(s);
-        boolean Result = m.matches();
-
-        if (Result == true){
-            System.out.println(output.valid);
+        System.out.print("Введите дату: ");
+        String date = input.nextLine();
+        class patterns{
+            static final String pattern = "^[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]";
+            static final String pattern1 = "(29|[3-9][0-9])/02/[0-2][0-9][0-9][0-9]";
+            static final String pattern2 = "^(0[1-9]|[1-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/(19[0-9][0-9]|[2-9][0-9][0-9][0-9])";
         }
-        else{
-            System.out.println(output.invalidFormat);
+        Pattern p1 = Pattern.compile(patterns.pattern1);
+        Matcher match1 = p1.matcher(date);
+        Pattern p = Pattern.compile(patterns.pattern);
+        Matcher match = p.matcher(date);
+        if (!(match.matches())) {
+            System.out.println("Введенное выражение не соответствует заданному формату даты dd/mm/yyyy");
+            System.exit(0);
+        }
+        Pattern p2 = Pattern.compile(patterns.pattern2);
+        Matcher match2 = p2.matcher(date);
+        if (match2.matches()) {
+            if(!match1.matches()) {
+                System.out.println("Данные корректны");
+            }
+            else {
+                System.out.println("Данные некорректны");
+            }
+
+        } else {
+            System.out.println("Данные некорректны");
         }
 
     }
