2f7c4f75de48d1af4aff19f2ad28cbf0e6db5bdd
4kith <mgolubevbiz@gmail.com>
2022-12-15 17:44:28 +0300

    update solution

--- src/laboratory_3/ex_2.java
+++ src/laboratory_3/ex_2.java
@@ -5,6 +5,7 @@ public class ex_2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Размерность массива: ");
+        while (true){
         int k = in.nextInt();
         int[] array = new int[k];
         for (int i = 0; i < k; i++) {
@@ -16,11 +17,17 @@ public class ex_2 {
         }
         System.out.print("\n"+"Число к удалению: ");
         int delete = in.nextInt();
-        System.out.print("Массив после обрабтки: ");
+        System.out.print("Массив после обработки: ");
         for (int m = 0; m < array.length; m++) {
                 if (array[m] != delete) {
                     System.out.print(array[m]+" ");
                 }
+        }
+            System.out.print("\n");
+            System.out.print("Введите размерность массива, либо выйдите из цикла используя /quit: ");
+        if(in.hasNext("quit")){
+            System.exit(0);
+        }
         }
     }
 }
