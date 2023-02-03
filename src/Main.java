07d70cbd59e3dcd140517e58dec103090b05d4f8
13fragment <mgolubevbiz@gmail.com>
2023-02-03 08:55:09 +0300

    update work

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-class Triangle{
+abstract class Figure{
+    public abstract double P();
+    public abstract double S();
+}
+class Triangle extends Figure{
     double a,b,c;
     public Triangle (double c,double b, double a){
         this.a = a;
@@ -16,11 +20,15 @@ class Triangle{
         return Math.sqrt(p*(p-a)*(p-b)*(p-c));
     }
 }
- class Rectangle{
+ class Rectangle extends Figure {
     double a,b;
-    public Rectangle (double b, double a){
+    public Rectangle (double b, double a) throws Exception {
+        if (a>=0 && b>=0){
         this.a = a;
         this.b = b;
+        }else{
+            throw new Exception ("Недопустимый параметр");
+        }
     }
     public String toString(){
         return "a = " + a + "b = " +b;
@@ -36,7 +44,11 @@ public class Main {
     public static void main(String[] args) {
         Triangle triangle = new Triangle(3,4,5);
         System.out.println(triangle.P());
-        Rectangle rectangle = new Rectangle(4,5);
+        try{
+        Rectangle rectangle = new Rectangle(4,8);
         System.out.println(rectangle.toString()+" S = "+rectangle.S());
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
     }
 }
