8587d9040f674b5ea99c23752327044819b43f14
13fragment <mgolubevbiz@gmail.com>
2023-02-17 06:58:02 +0300

    add division on zero Exception

--- src/laboratory_5/Ex.java
+++ src/laboratory_5/Ex.java
@@ -3,16 +3,22 @@ class Fractions{
     protected int[]fraction1={0,0};
     protected int[]fraction2 = {1,1};
 
-    public void setFractions(int numerator_1, int denominator_1){
+    public void setFractions(int numerator_1, int denominator_1)throws Exception{
         this.fraction1[0]=numerator_1;
         this.fraction1[1]=denominator_1;
+        if (denominator_1 == 0){
+            throw new Exception("Деление на 0");
+        }
     }
 
-    public void setFractions(int numerator_1, int denominator_1,int numerator_2, int denominator_2){
+    public void setFractions(int numerator_1, int denominator_1,int numerator_2, int denominator_2)throws Exception{
         this.fraction1[0]=numerator_1;
         this.fraction1[1]=denominator_1;
         this.fraction2[0]=numerator_2;
         this.fraction2[1]=denominator_2;
+        if (denominator_1 == 0|denominator_2==0){
+            throw new Exception("Деление на 0");
+        }
     }
     protected String summary(int[]fraction1){
         if (fraction1[0]==0){
@@ -74,10 +80,14 @@ class Fractions{
             return fraction1[0]+"/"+ fraction1[1];
         }
     }
-    protected String division(int[]fraction1,int[]fraction2){
+    protected String division(int[]fraction1,int[]fraction2)throws Exception{
         if (fraction1[0]==0){
             return "0";
-        }else{
+        }
+        if (fraction2[0] == 0){
+            throw new Exception("Деление на 0");
+        }
+        else{
             return fraction1[0]*fraction2[1]+"/"+ fraction1[1]*fraction2[0];
         }
     }
@@ -85,9 +95,16 @@ class Fractions{
 public class Ex {
     public static void main(String[] args) {
     Fractions fractions = new Fractions();
-        fractions.setFractions(1,8);
+        try{
+        fractions.setFractions(2,5);
         System.out.println(fractions.division(fractions.fraction1));
-        fractions.setFractions(7,8,9,6);
+        fractions.setFractions(1,0,2,6);
         System.out.println(fractions.division(fractions.fraction1, fractions.fraction2));
+        }catch (Exception e){
+            System.out.println("division on 0");
+        }
     }
+    /*
+    ДОБАВИТЬ ОБРАБОКТУ ОШИБКИ введенного неверного типа
+     */
 }
