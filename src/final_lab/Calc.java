b7695226b949436a49799070fb317e1fa0626a59
13fragment <mgolubevbiz@gmail.com>
2023-03-07 19:39:54 +0300

    add input condition method

--- src/final_lab/Calc.java
+++ src/final_lab/Calc.java
@@ -1,7 +1,18 @@
 package final_lab;
 
+import java.util.Scanner;
+
+class Fractions{
+    protected String getValue(){
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        in.close();
+    return s;
+    }
+}
 public class Calc {
     public static void main(String[] args) {
-
+        Fractions fractions = new Fractions();
+        System.out.println(fractions.getValue());
     }
 }
