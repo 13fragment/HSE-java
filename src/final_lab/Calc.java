a6e6f63407d6c5bffce14783b29cef8378baa842
13fragment <mgolubevbiz@gmail.com>
2023-03-16 19:20:54 +0300

    add sorting array method

--- src/final_lab/Calc.java
+++ src/final_lab/Calc.java
@@ -1,18 +1,22 @@
 package final_lab;
-
+import java.util.Arrays;
 import java.util.Scanner;
-
 class Fractions{
     protected String getValue(){
         Scanner in = new Scanner(System.in);
-        String s = in.next();
+        String s = in.nextLine();
         in.close();
     return s;
+    }
+    protected String [] sortElements(){
+        String [] g = getValue().split(" ");
+        return g;
     }
 }
 public class Calc {
     public static void main(String[] args) {
         Fractions fractions = new Fractions();
-        System.out.println(fractions.getValue());
+        System.out.println(Arrays.toString(fractions.sortElements()));
+//    1/2 + 1/3 = 5/6
     }
 }
